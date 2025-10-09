19cef717507480167b40d3ba052d710707745ba1
anjaleeDS <7247036+anjaleeDS@users.noreply.github.com>
2025-10-08 19:47:35 -0700

    Update make_post.py
    commented out "text" : {"format": {"type": "json"}} line 77

--- scripts/make_post.py
+++ scripts/make_post.py
@@ -74,7 +74,7 @@ def get_story_and_prompt():
                 {"role": "user", "content": user},
             ],
             # IMPORTANT:
-            "text": {"format": {"type": "json_object"}}
+            # "text": {"format": {"type": "json_object"}}
         },
         timeout=120
     )
